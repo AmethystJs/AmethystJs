84bebbf657a6bc8d0df873df0eae19c567172d51
AlecsDeveloper <120628011+AlecsDeveloper@users.noreply.github.com>
2023-08-22 19:55:50 -0500

    Actualizar main.js

--- src/scripts/BPM/@slash/src/main.js
+++ src/scripts/BPM/@slash/src/main.js
@@ -1,1 +1,19 @@
+import { world } from '@minecraft/server';
 
+export const Slash = {
+    Commands: {},
+    Register: options => Slash.Commands[options.name] = {
+        run: options.run
+    }
+}
+
+world.beforeEvents.chatSend.subscribe(ev => {
+    const { message } = ev;
+    let cmd = message.split(' ')[0].replace('./','');
+    if (!message.startsWith('./')) return;
+    ev.cancel = true;
+    if (!Slash.Commands[cmd]) return world.sendMessage(`§cInvalid command "${cmd}"`);
+    // Command
+    let snd = ev.sender, arg = message.split(' ').slice(1);
+    Slash.Commands[cmd].run(snd,arg);
+});
