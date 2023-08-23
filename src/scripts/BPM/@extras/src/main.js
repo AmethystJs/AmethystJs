0bd058b9f73f0dabddc13b79157b7f0b3ad126dd
AlecsDeveloper <120628011+AlecsDeveloper@users.noreply.github.com>
2023-08-22 19:56:57 -0500

    Actualizar main.js

--- src/scripts/BPM/@extras/src/main.js
+++ src/scripts/BPM/@extras/src/main.js
@@ -1,1 +1,24 @@
+import { system } from '@minecraft/server';
 
+// Others
+function sleep (ms) {
+    return new Promise(res => system.runTimeout(res,ms/50));
+}
+
+// Node
+function setTimeout(callback,ms) {
+    system.runTimeout(callback,ms/50);
+}
+function setInterval(callback,ms) {
+    return system.runInterval(callback,ms/50);
+}
+function clearInterval(interval) {
+    system.clearRun(interval)
+}
+
+
+// Exports
+export {
+    setTimeout, setInterval, clearInterval,
+    sleep
+}
